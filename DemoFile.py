26bdeca262197c0f47f7fe0f8f45be9316677906
DESKTOP-2LT3TL7\admin <dmltjd814@gmail.com>
2021-11-17 13:16:54 +0900

    문자열 연습 2

---
+++ DemoFile.py
@@ -0,0 +1,23 @@
+# DemoFile.py
+
+f = open("c:\\work\\demo.txt","wt",encoding="utf-8")
+f.write("첫번째라인\n두번째\n세번째\n")
+f.close()
+
+print( f.closed )
+
+#읽기 
+f = open("c:\\work\\demo.txt","rt",encoding="utf-8")
+print("---read()---")
+print( f.read() )
+print("---readline()---")
+#파일 포인터가 어디쯤?
+print( f.tell())
+f.seek(0)
+print(f.readline())
+#다중 라인 주석 처리: ctrl + /
+print(f.readline())
+print("---readlines()---")
+f.seek(0)
+print(f.readlines())
+
