79513d34f1a4a30721b4887db692bd09a55c2291
DESKTOP-2LT3TL7\admin <dmltjd814@gmail.com>
2021-11-18 11:06:49 +0900

    웹 클롤링 연습3

--- web1.py
+++ web1.py
@@ -16,4 +16,12 @@ soup = BeautifulSoup(page, "html.parser")
 
 #필터링: <p class = "outer-text">
 #파이썬에서 클래스를 정의하는 키워드 Class: Class_
-print( soup.find_all("p", class_ = "outer-text"))
+# print( soup.find_all("p", class_ = "outer-text"))
+#id 검색
+#print( soup.find_all(id="first") )
+
+for tag in soup.find_all("p"):
+    #컨텐츠만 달라:text속성
+    title = tag.text.strip()
+    title = title.replace("\t", "")
+    print(title)
